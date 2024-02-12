350f115f2144ae6b870b78172618df3efbaef90b
min56 <minchaeg85@gmail.com>
2024-02-12 22:08:45 +0900

    update :: UserEntity 추가

---
+++ auth-infrastructure/src/main/java/com/example/somauniversityauthserver/domain/user/domain/entity/UserEntity.java
@@ -0,0 +1,39 @@
+package com.example.somauniversityauthserver.domain.user.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "tbl_user")
+@Entity
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "VARCHAR(15)", nullable = false, unique = true)
+    private String accountId;
+
+    @Column(columnDefinition = "CHAR(256)", nullable = false)
+    private String password;
+
+    @Column(columnDefinition = "VARCHAR(50)", nullable = false, unique = true)
+    private String email;
+
+    @Column(columnDefinition = "VARCHAR(20)", nullable = false, unique = true)
+    private String nickName;
+
+    //TODO: Enum타입 School Column 추가
+}
