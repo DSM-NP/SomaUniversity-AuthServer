60b2e179167574792b5e8bce46f5c9f67ee909d3
min56 <minchaeg85@gmail.com>
2024-02-17 22:46:49 +0900

    add :: BaseIdEntity

---
+++ auth-infrastructure/src/main/java/com/example/somauniversityauthserver/domain/global/BaseIdEntity.java
@@ -0,0 +1,27 @@
+package com.example.somauniversityauthserver.domain.global;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@SuperBuilder
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseIdEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
