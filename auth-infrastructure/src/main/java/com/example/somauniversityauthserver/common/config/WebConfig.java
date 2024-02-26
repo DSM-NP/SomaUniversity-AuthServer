9c721d513c52b162171af1eaa3d6f0de639df73e
min56 <minchaeg85@gmail.com>
2024-02-26 22:15:41 +0900

    update :: 패키지 변경

--- auth-infrastructure/src/main/java/com/example/somauniversityauthserver/common/config/WebConfig.java
+++ auth-infrastructure/src/main/java/com/example/somauniversityauthserver/common/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.somauniversityauthserver.common.security;
+package com.example.somauniversityauthserver.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
